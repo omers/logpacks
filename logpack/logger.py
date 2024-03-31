a69467d9da97bbac11a05e4c723634994c4338a3
Omer Segev <omers@aidoc.com>
2024-03-31 15:27:19 +0300

    Added docstrings

--- logpack/logger.py
+++ logpack/logger.py
@@ -1,14 +1,35 @@
+"""Module providing logging capabilities."""
+
 import logging
 import re
 import sys
 
 
 class Logger:
+    """
+    A simple logger class for handling application logging.
+    """
+
     def __init__(self, log_file="app.log"):
+        """
+        Initializes the Logger object.
+
+        Parameters:
+        - log_file (str): The path to the log file. Default is "app.log".
+        """
         self.log_file = log_file
         self.setup_logging()
 
     def _obfuscate_message(self, message):
+        """
+        Obfuscates sensitive information in the message.
+
+        Parameters:
+        - message (str): The message to obfuscate.
+
+        Returns:
+        - str: The obfuscated message.
+        """
         sensitive_patterns = [
             (r"\b(?!000|.+0{4})(?:\d{9}|\d{3}-\d{2}-\d{4})\b", "[REDACT]"),
             (r"\b\d{4}-\d{4}-\d{4}-\d{4}\b", "[REDACT]"),
@@ -23,17 +44,40 @@ class Logger:
         return message
 
     def setup_logging(self):
-        log_format = '{"timestamp": "%(asctime)s", "loglevel": "%(levelname)s", "message": "%(message)s"}'
+        """
+        Sets up logging configuration.
+        """
+        log_format = '{"timestamp": "%(asctime)s",\
+                       "loglevel": "%(levelname)s",\
+                       "message": "%(message)s"}'
         logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=log_format)
 
     def log_info(self, message):
+        """
+        Logs an informational message.
+
+        Parameters:
+        - message (str): The message to log.
+        """
         message = self._obfuscate_message(message)
         logging.info(message)
 
     def log_error(self, message):
+        """
+        Logs an error message.
+
+        Parameters:
+        - message (str): The message to log.
+        """
         message = self._obfuscate_message(message)
         logging.error(message)
 
     def log_warning(self, message):
+        """
+        Logs a warning message.
+
+        Parameters:
+        - message (str): The message to log.
+        """
         message = self._obfuscate_message(message)
         logging.warning(message)
